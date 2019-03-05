0a339e063477c1484fa658059b7e6a691e894d63
Eli Wemyss <eliwemyss@gmail.com>
2019-03-04 16:09:14 -0800

    finalizing styles and defined colors

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+  let checked = $("#menu").prop("checked");
+  //Close navigation bar if user clicks outside
+  $(".content").on("click", () => {
+    if (!checked && screen.width < 768) {
+      $(".menu-content").css("max-height", 0);
+      $("#menu").prop("checked", false);
+    }
+  });
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 50) {        
+        $('#return-to-top').fadeIn(200);    
+    } else {
+        $('#return-to-top').fadeOut(200);   
+    }
+});
+$('#return-to-top').click(function() {      
+    $('body,html').animate({
+        scrollTop : 0                       
+    }, 500);
+});
